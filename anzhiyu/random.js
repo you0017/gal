79da8ce6e68d9e7df04468c3c8f575ebec6fa4c9
zhangeh <2092425375@qq.com>
2025-01-18 18:00:24 +0800

    Site updated: 2025-01-18 18:00:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/18/hello-world/","2024/09/09/gal/"];function toRandomPost(){
+var posts=["2024/09/09/gal/","2025/01/18/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
