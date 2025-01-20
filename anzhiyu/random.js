bdab7af75dea1bb41541e001f91a0ee4c9472ff7
zhangeh <2092425375@qq.com>
2025-01-20 15:48:25 +0800

    Site updated: 2025-01-20 15:48:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/09/parse/【解析】Butterfly Seeker/","2025/01/20/source/pc/【PC】灵感满溢的甜蜜创想/","2025/01/20/source/kr/【Ty】灵感满溢的甜蜜创想/"];function toRandomPost(){
+var posts=["2024/09/09/parse/【解析】Butterfly Seeker/","2025/01/20/source/kr/【Ty】灵感满溢的甜蜜创想/","2025/01/20/source/pc/【PC】灵感满溢的甜蜜创想/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
